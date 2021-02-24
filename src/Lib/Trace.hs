c464a95694c72d3aa480be6a531c5258dc8744ac
stevenfontanella <steven.fontanella@gmail.com>
2021-02-23 16:20:43 -0800

    trace functions

--- src/Lib/Trace.hs
+++ src/Lib/Trace.hs
@@ -9,7 +9,7 @@
     Debug module to insert 'trace' statements.
 -}
 -----------------------------------------------------------------------------
-module Lib.Trace( trace, traceDoc, ctrace, Color(..) ) where
+module Lib.Trace( trace, traceDoc, ctrace, Color(..), traceShowId, traceShow, traceM, traceShowM ) where
 
 import Lib.Printer
 import Lib.PPrint
@@ -18,6 +18,20 @@ import Platform.Runtime( unsafePerformIO )
 trace :: String -> a -> a
 trace msg x
   = ctrace DarkGray msg x 
+
+traceShowM :: (Applicative m, Show s) => s -> m ()
+traceShowM msg = ctrace DarkGray (show msg) $ pure ()
+
+traceM :: (Applicative m) => String -> m ()
+traceM msg = ctrace DarkGray msg $ pure ()
+
+traceShow :: (Show s) => s -> a -> a
+traceShow s a
+  = ctrace DarkGray (show s) a
+
+traceShowId :: (Show a) => a -> a
+traceShowId x
+  = ctrace DarkGray (show x) x
 
 traceDoc :: Doc -> a -> a
 traceDoc msg x
